41c1cefbc46960d3ef43627ab7aec73a0ccea565
Rishi Kumra <rishikumra01@gmail.com>
2025-05-14 15:14:47 -0700

    Initial Commit

---
+++ OwlTrace/OwlTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwlTraceApp.swift
+//  OwlTrace
+//
+//  Created by Rishi Kumra on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct OwlTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OwlTraceTests/OwlTraceTests.swift
@@ -0,0 +1,17 @@
+//
+//  OwlTraceTests.swift
+//  OwlTraceTests
+//
+//  Created by Rishi Kumra on 5/14/25.
+//
+
+import Testing
+@testable import OwlTrace
+
+struct OwlTraceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
